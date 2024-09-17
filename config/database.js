f4513bd602bd0918db9de85005c9047d871bebf5
ALMA3uk <alma3uk7@gmail.com>
2024-09-17 22:18:05 +0300

    Change sequelize connection settings
    - Removed logging parameter
    - Changed connection parameters to environment variables instead of strings

--- config/database.js
+++ config/database.js
@@ -1,10 +1,9 @@
 require('dotenv').config();
 const { Sequelize } = require('sequelize');
 
-const sequelize = new Sequelize('node', 'root', 'root', {
+const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
     host: process.env.DB_HOST || 'localhost',
-    dialect: 'mysql',
-    logging: false
+    dialect: 'mysql'
 });
 
 const User = require('../models/User')(sequelize);
